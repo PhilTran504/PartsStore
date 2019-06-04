917e9a1aa7556ea7f498ac423480f303890f7f72
Ustolemyrice <phil_504@yahoo.com>
2019-06-03 19:23:03 -0500

    Add project files.

---
+++ PartsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartsStore.Models
+{
+    public class FakeProductRepository /* : IProductRepository */
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product {ProductName = "Supra Exhaust", Price = 1500},
+            new Product {ProductName = "FPGreen Turbo", Price = 1800},
+            new Product {ProductName = "Work Wheels", Price = 3200}
+        }.AsQueryable<Product>();
+    }
+}
---
+++ PartsStore/Models/SeedData.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace PartsStore.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            ApplicationDbContext context = app.ApplicationServices
+                .GetRequiredService<ApplicationDbContext>();
+            context.Database.Migrate();
+            if (!context.Products.Any())
+                {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        ProductName = "2013 Toyota FR-S Exhaust", Description = "Exhaust for Toyota FR-S",
+                        Category = "Toyota", Price = 1300
+                    },
+                    new Product
+                    {
+                        ProductName = "2020 Toyota Supra Coilover",
+                        Description = "Supra Coilover",
+                        Category = "Toyota",
+                        Price = 1000
+                    },
+                    new Product
+                    {
+                        ProductName = "2013 Subarau WRX Exhaust",
+                        Description = "Exhaust for Subaru WRX",
+                        Category = "Subaru",
+                        Price = 1200
+                    },
+                    new Product
+                    {
+                        ProductName = "2004-2006 Subaru WRX Turbo kit",
+                        Description = "Full Turbo kit for Subaru WRX",
+                        Category = "Subaru",
+                        Price = 4500
+                    },
+                    new Product
+                    {
+                        ProductName = "2018 Subaru WRX Short Shifter",
+                        Description = "Lowers the shifting throw",
+                        Category = "Subaru",
+                        Price = 67
+                    },
+                    new Product
+                    {
+                        ProductName = "Porsche 718 Cayman Sport Seats",
+                        Description = "Sport-styled seats for the new 718",
+                        Category = "Porsche",
+                        Price = 2300
+                    },
+                    new Product
+                    {
+                        ProductName = "2018 Porsche GT3 Wing",
+                        Description = "Rear wing for new Porsche GT3",
+                        Category = "Porsche",
+                        Price = 1000
+                    },
+                    new Product
+                    {
+                        ProductName = "Porsche 718 Cayman Factory Wheels",
+                        Description = "Factory wheels for 718 Cayman",
+                        Category = "Porsche",
+                        Price = 2600
+                    },
+                    new Product
+                    {
+                        ProductName = "Porsche GT3 Exhaust",
+                        Description = "Exhaust from GT3",
+                        Category = "Porsche",
+                        Price = 2000
+                    
+                    }
+                    );
+                    context.SaveChanges();
+            }
+
+        }
+    }
+}
